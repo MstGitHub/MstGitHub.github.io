e0b8657307107987c9cd0f41c1efe1358d4d3a14
maoshengtao <maoshengtao@gagogroup.com>
2020-02-04 17:32:28 +0800

    feat: home

---
+++ precache-manifest.7d3bf6a3307066f4122b25c62cbba3d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa82947cdea59e69356c93540065e191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c43d05b8b27360ee469",
+    "url": "/static/css/main.8dbb59cb.chunk.css"
+  },
+  {
+    "revision": "f69247021e31c15d2ba7",
+    "url": "/static/js/2.d18ce34a.chunk.js"
+  },
+  {
+    "revision": "9c43d05b8b27360ee469",
+    "url": "/static/js/main.43bfb90d.chunk.js"
+  },
+  {
+    "revision": "a78d8732c33091f38a12",
+    "url": "/static/js/runtime-main.b8d3e742.js"
+  },
+  {
+    "revision": "b43e4573aee3ba9df16638ef3b2ba3b8",
+    "url": "/static/media/1.b43e4573.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "583ce7b438d30b0024697f9c0429499c",
+    "url": "/static/media/logo.583ce7b4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a22e28ac98f11fd03f4ce8bb9fef1098.js"
+  "/precache-manifest.7d3bf6a3307066f4122b25c62cbba3d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

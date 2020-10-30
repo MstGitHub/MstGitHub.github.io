b0429771d1af60af092d3d7b0ac6cdc9918bdc2c
maoshengtao <maoshengtao@gagogroup.com>
2020-10-30 16:34:12 +0800

    feat: 更新了一些 router使用相关

--- mst-react-ant/src/App.js
+++ mst-react-ant/src/App.js
@@ -2,27 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/home";
-
+import { OrigiRoute } from "./pages/origin-route";
+import { DemoReactRoute } from "./pages/react-route";
+import { TodoList } from "./pages/todo-list";
 function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* <Home/> */}
+      {/* <OrigiRoute /> */}
+      <DemoReactRoute/>
+      <TodoList/>
     </div>
   );
 }
+
 
 export default App;
